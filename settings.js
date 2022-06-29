7c6f76c8aa64468d13e53edaa6f8f5fb197b627d
ReyBotzOfc <107416169+ReyBotzOfc@users.noreply.github.com>
2022-06-29 16:50:19 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,28 +12,28 @@ global.APIKeys = {
 
 //===> ATUR BIAR KALIAN SENENG
 global.autoReadGc = true //Ubah Ke false Jika Chat Di Grup Tidak Ingin Di Baca Oleh Bot
-global.autoReadAll = false //Ubah Ke true Jika Ingin Seluruh Chat Di Baca Oleh Bot
+global.autoReadAll = true //Ubah Ke true Jika Ingin Seluruh Chat Di Baca Oleh Bot
 global.anticall = true //Ubah Ke false Jika Tidak Ingin Bot Blockir Yang Telepon
 global.available = false //Ubah Ke false Jika Bot Mu Tidak Ingin Terlihat Online
-global.autoTyping = false //Ubah Ke true Jika Ingin Saat Ada Yang Chat Bot Auto Mengetik
-global.autoRecord = true //Ubah Ke false Jika Tidak Ingin Saat Ada Yang Chat Bot Auto Merekam
+global.autoTyping = true //Ubah Ke true Jika Ingin Saat Ada Yang Chat Bot Auto Mengetik
+global.autoRecord = false //Ubah Ke false Jika Tidak Ingin Saat Ada Yang Chat Bot Auto Merekam
 global.multiplier = 69 // Semakin Besar Angkanya Semakin Sulit Untuk Naik Level
 global.limitCount = 70 //70 Dah Pas Banh
 global.prefa = ['','.','/','#']
 
 //===> UBAH DISINI
-global.namaowner = 'Ryuuka Team' //Ganti Jadi Namamu
-global.owner = ['6285609233482','6283891921787'] //Kalo Mau Lebih Tambahin Aja
-global.nomerowner = '6285609233482' //Owner Utama Istilahnya
-global.namabot = 'Hiruko Kagetane' //Ganti Jadi Nama Botmu
-global.packname = 'Hiruko Kagetane' //Sticker Weem
-global.author = 'By Ryuuka Team' //Sticker Weem
-global.webme = 'https://api-ryubotz.herokuapp.com' //Ganti Sama Web Mu
-global.linkgroup = 'https://chat.whatsapp.com/BhOYdoWNRFC4n4bdsEexlV' //Ganti Sama Link Gc Mu
+global.namaowner = 'KoKoRey Team' //Ganti Jadi Namamu
+global.owner = ['6285232604817','6285232604817'] //Kalo Mau Lebih Tambahin Aja
+global.nomerowner = '6285232604817' //Owner Utama Istilahnya
+global.namabot = 'I m BotRey' //Ganti Jadi Nama Botmu
+global.packname = 'ItsMe Rey' //Sticker Weem
+global.author = 'By ®.KoKoRey' //Sticker Weem
+global.webme = 'https://instagram.com/callme_rey1921' //Ganti Sama Web Mu
+global.linkgroup = 'https://instagram.com/callme_rey1921' //Ganti Sama Link Gc Mu
 global.sessionName = 'session'
-global.youtube = 'https://youtube.com/c/RyuukaBotz' //Ganti Sama Link Yt Mu
-global.github = 'https://github.com/R-TEAM94' //Ganti Sama Link Github Mu
-global.instagram = 'https://instagram.com/fandyyy._' //Ganti Sama Link Ig mu
+global.youtube = 'https://instagram.com/callme_rey1921' //Ganti Sama Link Yt Mu
+global.github = 'https://instagram.com/callme_rey1921' //Ganti Sama Link Github Mu
+global.instagram = 'https://instagram.com/callme_rey1921._' //Ganti Sama Link Ig mu
 
 //===> BACKGROUND SETIAP MENU
 global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -60,10 +60,10 @@ limitEnd: 'Limit Anda Habis\n\n📝 Note : Limit Akan Di Reset Setiap Jam 21:00'
 
 //===> EDIT SESUAI KEMAUANMU
 global.mess = {
-admin: '*LU ADMIN? 🤨*',
-botAdmin: '*BOT BUKAN ADMIN 😔*',
-owner: '*LU BUKAN OWNER GW 😠*',
-group: '*KHUSUS GROUP 😏*',
+admin: '*LU ADMIN?*',
+botAdmin: '*GUA BUKAN ADMIN*',
+owner: '*LU BUKAN OWNER GW_-*',
+group: '*KHUSUS GROUP ABWANG TETEH*',
 private: '*KHUSUS PRIVATE CHAT*',
 bot: '*KHUSUS PENGGUNA NOMER BOT*',
 wait: '*SABAR SEDANG DI PROSES ...*',
